c65c3c05c071b32b433edbf549e92262ff2bb584
Sergey Krashevich <svk@svk.su>
2023-07-29 14:19:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 nginx -g 'pid /tmp/nginx.pid;' -c '/nginx.conf'
 mkdir -p '/run/dump1090'
-/usr/bin/dump1090 --quiet --net --write-json '/run/dump1090'
+/usr/bin/dump1090 --quiet --net --fix --write-json '/run/dump1090'
